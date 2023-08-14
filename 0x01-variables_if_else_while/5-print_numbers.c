7a116cc8751987bdd81f7ab62fe3101392316e36
chaimaeboultam <chaimaeboultam2002@gmail.com>
2023-08-14 12:31:08 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,8 +5,11 @@
  * Return: always 0
  */
 
-int main() {
-    for (int i = 0; i < 10; i++) {
+int main(void)
+{
+    int i;
+
+    for (i = 0; i < 10; i++) {
         putchar(i + '0');
     }
     putchar('\n');
