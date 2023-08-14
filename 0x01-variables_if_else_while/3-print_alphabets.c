362b36fba0570e1ba0669937150b2b74015f0662
chaimaeboultam <chaimaeboultam2002@gmail.com>
2023-08-14 10:43:43 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,23 +2,25 @@
 
 /**
  * main - Entry point
- * Description: 'Printing the alphabet in lowercase'
+ * Description: Printing the alphabet in lowercase and uppercase
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-  char alpha = 'a';
+    char alpha = 'a';
 
-  do {
-    putchar(alpha);
-    alpha++;
-  } while (alpha <= 'z');
-  char alpho='A';
+    do {
+        putchar(alpha);
+        alpha++;
+    } while (alpha <= 'z');
 
-  do{
-    putchar(alpho);
-    alpho++;
-  }while(alpho <= 'Z');
-  printf("\n");
-  return (0);
+    char alpho = 'A';
+
+    do {
+        putchar(alpho);
+        alpho++;
+    } while (alpho <= 'Z');
+
+    printf("\n");
+    return (0);
 }
