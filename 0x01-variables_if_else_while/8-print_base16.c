ad7f59c051c7ebcba7940f6fdc4ab3151074753c
chaimaeboultam <chaimaeboultam2002@gmail.com>
2023-08-14 12:57:45 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'all the numbers of base 16 in lowercase'
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	for(i=0;i<16;i++){
+		if(i<10)
+			putchar(i + '0');
+		else
+			putchar(i-10 + 'a');
+	}
+	return 0;
+}
