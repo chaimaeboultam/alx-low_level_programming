fa415113a67db1b38ac590f971965205cfb16f42
chaimaeboultam <chaimaeboultam2002@gmail.com>
2023-08-14 13:14:01 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,12 +2,14 @@
 #include <stdlib.h>
 
 int main(void)
+
 {
     int i;
 
     for (i = 1; i < 9; i++) {
         putchar(i + '0');
         putchar(',');
+	putchar(' ');
     }
     putchar('9');
     putchar('\n');
