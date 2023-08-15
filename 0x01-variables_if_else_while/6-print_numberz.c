29352b6f219e664fdda58f3f029c1a848e2202cd
chaimaeboultam <chaimaeboultam2002@gmail.com>
2023-08-15 08:50:20 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
- * Description: 'all single digit numbers of base 10 starting from 0'
- * Return: always 0
+ *
+ * Description: Prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: Always 0
  */
-
 int main(void)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < 10; i++) {
-        putchar(i + '0');
-    }
-    putchar('\n');
-    return 0;
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+
+	putchar('\n');
+	return (0);
 }
