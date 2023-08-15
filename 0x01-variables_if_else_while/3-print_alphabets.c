1bb201a7cc480e26a92fd9c18475dcf6e5d9254c
chaimaeboultam <chaimaeboultam2002@gmail.com>
2023-08-15 08:39:16 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,24 +2,26 @@
 
 /**
  * main - Entry point
- * Description: Printing the alphabet in lowercase and uppercase
+ *
+ * Description: Prints the alphabet in lowercase and uppercase
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    char alpha = 'a';
-    char alpho = 'A';
+	char alpha = 'a';
+	char alpho = 'A';
 
-    do {
-        putchar(alpha);
-        alpha++;
-    } while (alpha <= 'z');
+	do {
+		putchar(alpha);
+		alpha++;
+	} while (alpha <= 'z');
 
-    do {
-        putchar(alpho);
-        alpho++;
-    } while (alpho <= 'Z');
+	do {
+		putchar(alpho);
+		alpho++;
+	} while (alpho <= 'Z');
 
-    putchar('\n');
-    return (0);
+	putchar('\n');
+	return (0);
 }
