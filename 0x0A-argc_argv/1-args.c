f61de4ee767adf4ca5893b6b2c113e183af4134e
chaimaeboultam <chaimaeboultam2002@gmail.com>
2023-09-08 07:05:34 -0700

    .

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main-prints the number of arguments passed into it
+ * @argc:number of arguments
+ * @argv:array of strings
+ *
+ * return:0 always succes
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc-1);
+
+	return (0);
+}
