e21ebb08689b9549764bca9442a7e6a7b18f724f
chaimaeboultam <chaimaeboultam2002@gmail.com>
2023-09-09 08:39:47 -0700

    .

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,35 @@
 #include <stdio.h>
-#include <stdlib.h> // You need this for atoi
+
+/**
+ * _atoi - Convert a string to an integer
+ * @s: String to be converted
+ *
+ * Return: The converted integer
+ */
+int _atoi(char *s)
+{
+    int result = 0;
+    int sign = 1;
+
+    /* Ignore leading white space characters */
+    while (*s == ' ')
+        s++;
+
+    if (*s == '-' || *s == '+')
+    {
+        sign = (*s == '-') ? -1 : 1;
+        s++;
+    }
+
+    while (*s >= '0' && *s <= '9')
+    {
+        result = result * 10 + (*s - '0');
+        s++;
+    }
+
+    return sign * result;
+}
+
 
 /**
  * main - Multiplies two numbers
@@ -19,8 +49,8 @@ int main(int argc, char *argv[])
     }
     else
     {
-        num1 = atoi(argv[1]);
-        num2 = atoi(argv[2]);
+        num1 = _atoi(argv[1]);
+        num2 = _atoi(argv[2]);
         mul = num1 * num2;
         printf("%d\n", mul);
     }
