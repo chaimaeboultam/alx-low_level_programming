ec96616ade85805433636b71c16333a457a86185
chaimaeboultam <chaimaeboultam2002@gmail.com>
2023-09-09 06:50:43 -0700

    .

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
-#include <stdlib.h>
-
+#include <stdlib.h> // You need this for atoi
 
 /**
- * main-multiplies two numbers
- * @argc:number of arguments
- * @argv:array of string
+ * main - Multiplies two numbers
+ * @argc: Number of arguments
+ * @argv: Array of arguments
  *
- * return: 0 always success
+ * Return: 0 on success, 1 on error
  */
 int main(int argc, char *argv[])
 {
-	int mul, num1, num2;
-	if(argc != 3)
-	{
-		printf("ERROR\n");
-		return 1;
-	}
-	else
-	{
-		num1= atoi(arg[1]);
-		num2= atoi(arg[2]);
-		mul= num1 * num2;
-		printf("%d\n", mul);
-	}
-	
-	return (0);
+    int mul, num1, num2;
+
+    if (argc != 3)
+    {
+        printf("ERROR\n");
+        return 1;
+    }
+    else
+    {
+        num1 = atoi(argv[1]);
+        num2 = atoi(argv[2]);
+        mul = num1 * num2;
+        printf("%d\n", mul);
+    }
+
+    return 0;
 }
