c8b991faa4acaeb74a7e75aa93c9ea32770f3d6e
chaimaeboultam <chaimaeboultam2002@gmail.com>
2023-09-09 06:46:45 -0700

    .

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+
+/**
+ * main-multiplies two numbers
+ * @argc:number of arguments
+ * @argv:array of string
+ *
+ * return: 0 always success
+ */
+int main(int argc, char *argv[])
+{
+	int mul, num1, num2;
+	if(argc<3 || argc>3)
+	{
+		printf("ERROR\n");
+		return 1;
+	}
+	else
+	{
+		num1= atoi(arg[1]);
+		num2= atoi(arg[2]);
+		mul= num1 * num2;
+		printf("%d\n", mul);
+	}
+	
+	return (0);
+}
