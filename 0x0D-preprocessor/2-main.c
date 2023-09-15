bdb9e2190936d5112ede173b6ba0c6bfd6e31ec6
chaimae boultam <chaimaeboultam2002@gmail.com>
2023-09-15 08:17:53 -0700

    .

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point, prints the name of the file it was compiled from
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    _putchar(__FILE__[0]);
+    _putchar(__FILE__[1]);
+    _putchar('\n');
+
+    return (0);
+}
