1adb286cce6312d4d2e36e81cdd2feae0372a08f
chaimae boultam <chaimaeboultam2002@gmail.com>
2023-09-15 13:41:57 -0700

    .

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * init_dog-a function that initialize a variable of type struct dog
+ * @d: a pointer to the structre that will be initialize
+ * @name: name to initialize
+ * @age: age to initialize
+ * @owner: owner to initialize
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	p->name = name;
+	p->age = age;
+	p->owner = owner;
+}
