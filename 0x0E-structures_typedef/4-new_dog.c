1dcb8c688f1e28b955c0e2ce65ef6d4384b1b9b3
chaimae boultam <chaimaeboultam2002@gmail.com>
2023-09-16 07:59:38 -0700

    .

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>  // Added for malloc
+#include "dog.h"
+
+/**
+ * _strlen - Calculate the length of a string
+ * @str: The input string
+ *
+ * Return: The length of the string
+ */
+int _strlen(char *str)
+{
+    int len = 0;
+
+    while (str[len] != '\0')
+        len++;
+
+    return (len);
+}
+
+/**
+ * _strcopy - Copy a string
+ * @dest: The destination string
+ * @src: The source string
+ */
+void _strcopy(char *dest, char *src)
+{
+    int i = 0;
+
+    while (src[i] != '\0')
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+}
+
+/**
+ * new_dog - Create a new dog
+ * @name: The name of the new dog
+ * @age: The age of the new dog
+ * @owner: The owner of the new dog
+ *
+ * Return: A pointer to the new dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+    dog_t *new_dog;
+
+    // Check if dog_t is NULL
+    if (!dog_t)
+        return (NULL);
+
+    int name_len = _strlen(name);
+    int owner_len = _strlen(owner);
+
+    new_dog = malloc(sizeof(dog_t));
+
+    if (new_dog == NULL)
+        return (NULL);
+
+    new_dog->name = malloc(name_len + 1);
+    new_dog->owner = malloc(owner_len + 1);
+
+    if (new_dog->name == NULL || new_dog->owner == NULL)
+    {
+        free(new_dog->name);
+        free(new_dog->owner);
+        free(new_dog);
+        return (NULL);
+    }
+
+    _strcopy(new_dog->name, name);
+    _strcopy(new_dog->owner, owner);
+
+    new_dog->age = age;
+
+    printf("My name is: %s, and I am: %.1f\n", new_dog->name, new_dog->age);
+
+    return new_dog;
+}
