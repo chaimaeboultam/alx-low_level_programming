8c3c67cf0ced2c0ca2bc0e37201f6c719f8db69f
chaimae boultam <chaimaeboultam2002@gmail.com>
2023-09-17 14:14:42 -0700

    .

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,7 +12,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if(arrar == NULL || size == NULL || action == NULL)
+	if(arrar == NULL || action == NULL)
 		return;
 	
 	for (i = 0; i < size; i++)
