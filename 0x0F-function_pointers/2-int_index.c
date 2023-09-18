a710548ec00774fce775f9016f8460300e28aa5d
chaimae boultam <chaimaeboultam2002@gmail.com>
2023-09-18 05:29:23 -0700

    /

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -20,8 +20,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 	for (int i = 0; i < size; i++)
 	{
         	if (cmp(array[i]) != 0)
-        	return i;
+        	return (i);
        	}
 
-	return -1;
+	return (-1);
 }
