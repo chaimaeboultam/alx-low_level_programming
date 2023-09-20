d928d3b9223ae9e0226b39173da0d0229fd427ea
chaimae boultam <chaimaeboultam2002@gmail.com>
2023-09-20 06:05:00 -0700

    .

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -15,16 +15,14 @@ int sum_them_all(const unsigned int n, ...)
 
 	if (n == 0)
 		return (0);
-	else
-	{
-		va_list args;
-		va_start(args, n);
+	
+	va_list args;
+	va_start(args, n);
 		
-		for (i = 0; i < n; i++)
-			sum += va_arg(args, int);
+	for (i = 0; i < n; i++)
+		sum += va_arg(args, int);
 
-		va_end(args);
-	}
+	va_end(args);
 
 	return (sum);
 }
